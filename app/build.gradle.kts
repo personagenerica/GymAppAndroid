53691ba5f2b67768a598ee69301d07e9c9da3b4a
Anonimus <169140637+personagenerica@users.noreply.github.com>
2026-03-02 13:23:18 +0100

    Home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
+    implementation("com.squareup.picasso:picasso:2.8")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10")
--- app/src/main/java/com/gymapp/MainActivity.java
+++ app/src/main/java/com/gymapp/MainActivity.java
@@ -8,28 +8,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.gymapp.model.Actor;
-import com.gymapp.model.Admin;
-import com.gymapp.model.Monitor;
-import com.gymapp.model.Rol;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.gymapp.model.Usuario;
 import com.gymapp.services.ActorService;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
-
 
     private ActorService actorService;
 
@@ -38,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        ImageView imgHome = findViewById(R.id.imgHome);
+
+        Picasso.get()
+                .load("https://p.turbosquid.com/ts-thumb/bC/eqDLdW/CX/gym1/png/1626180632/600x600/fit_q87/f92c2643c008ad3ff5e65e80700d1e0975200641/gym1.jpg")
+                .into(imgHome);
         // ====== SharedPreferences ======
         SharedPreferences prefs = getSharedPreferences("auth_prefs", MODE_PRIVATE);
         String rol = prefs.getString("rol", "");
@@ -51,11 +42,16 @@ public class MainActivity extends AppCompatActivity {
 
         bottomMenu.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
-            if (id == R.id.navigation_home) return true;
+
+            if (id == R.id.navigation_home) {
+                return true;
+            }
+
             if (id == R.id.navigation_clases) {
                 startActivity(new Intent(this, ReservarClasesActivity.class));
                 return true;
             }
+
             if (id == R.id.navigation_productos) {
                 startActivity(new Intent(this, ProductoActivity.class));
                 return true;
@@ -73,106 +69,12 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(this, WelcomeActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
                 startActivity(intent);
                 finish();
+                return true;
             }
+
             return false;
         });
-    }
-
-    // =========================
-    // CREAR USUARIOS Y MONITORES
-    // =========================
-    private void crearUsuariosYMonitores() {
-
-        // 3 usuarios
-        List<Usuario> usuarios = new ArrayList<>(Arrays.asList(
-                new Usuario("Usuario1", "user1", "Apellido1", "user1@email.com",
-                        null, "612345678", 25, null, "1234"),
-                new Usuario("Usuario2", "user2", "Apellido2", "user2@email.com",
-                        null, "612345679", 30, null, "1234"),
-                new Usuario("Usuario3", "user3", "Apellido3", "user3@email.com",
-                        null, "612345680", 28, null, "1234")
-        ));
-
-        // 3 monitores
-        List<Monitor> monitores = Arrays.asList(
-                new Monitor("Monitor1", "monitor1", "ApellidoM1", "monitor1@email.com",
-                        null, "698765432", 35, Rol.Monitor, "1234"),
-                new Monitor("Monitor2", "monitor2", "ApellidoM2", "monitor2@email.com",
-                        null, "698765433", 40, Rol.Monitor, "1234"),
-                new Monitor("Monitor3", "monitor3", "ApellidoM3", "monitor3@email.com",
-                        null, "698765434", 38, Rol.Monitor, "1234")
-        );
-
-        List<Admin> admins = Arrays.asList(
-                new Admin("admin", "admin", "ApellidoA1", "admin@email.com",
-                        null, "698765432", 35, Rol.Admin
-                        , "1234")
-        );
-
-
-        StringBuilder resumen = new StringBuilder("Creación de actores:\n");
-
-        for (Actor a : usuarios) {
-            actorService.registrar((Usuario) a).enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    if (response.isSuccessful()) {
-                        resumen.append(a.getUsername()).append(" ✅\n");
-                    } else {
-                        resumen.append(a.getUsername()).append(" ❌\n");
-                    }
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    resumen.append(a.getUsername()).append(" ⚠\n");
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-
-        for (Monitor m : monitores) {
-            actorService.registrar(m).enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    if (response.isSuccessful()) {
-                        resumen.append(m.getUsername()).append(" ✅\n");
-                    } else {
-                        resumen.append(m.getUsername()).append(" ❌\n");
-                    }
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    resumen.append(m.getUsername()).append(" ⚠\n");
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
-        //Admin
-        for (Admin a : admins) {
-            actorService.registrar(a).enqueue(new Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    if (response.isSuccessful()) {
-                        resumen.append(a.getUsername()).append(" ✅\n");
-                    } else {
-                        resumen.append(a.getUsername()).append(" ❌\n");
-                    }
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    resumen.append(a.getUsername()).append(" ⚠\n");
-                    Toast.makeText(MainActivity.this, resumen.toString(), Toast.LENGTH_LONG).show();
-                }
-            });
-        }
     }
 }

e841f01d314c74de4f8d97a9a9a89d1b81693463
Adrian <169140637+personagenerica@users.noreply.github.com>
2026-03-03 12:25:16 +0100

    Version funcionando de clases a falta de fecha y hora

--- app/src/main/java/com/gymapp/model/Clase.java
+++ app/src/main/java/com/gymapp/model/Clase.java
@@ -1,14 +1,13 @@
 package com.gymapp.model;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class Clase implements Serializable {
 
     private int id;
-    private LocalDateTime fechaInicio;
-    private LocalDateTime fechaFin;
+    private String fechaInicio;
+    private String fechaFin;
     private int aforo;
     private List<Usuario> usuarios;
     private Monitor monitor; // ahora es el monitor completo
@@ -19,11 +18,11 @@ public class Clase implements Serializable {
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public LocalDateTime getFechaInicio() { return fechaInicio; }
-    public void setFechaInicio(LocalDateTime fechaInicio) { this.fechaInicio = fechaInicio; }
+    public String getFechaInicio() { return fechaInicio; }
+    public void setFechaInicio(String fechaInicio) { this.fechaInicio = fechaInicio; }
 
-    public LocalDateTime getFechaFin() { return fechaFin; }
-    public void setFechaFin(LocalDateTime fechaFin) { this.fechaFin = fechaFin; }
+    public String getFechaFin() { return fechaFin; }
+    public void setFechaFin(String fechaFin) { this.fechaFin = fechaFin; }
 
     public int getAforo() { return aforo; }
     public void setAforo(int aforo) { this.aforo = aforo; }
